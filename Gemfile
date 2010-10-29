d9c65430ac9ff6101b97d6f0bd537d3245e7cb20
Igor Afonov <afonov@gmail.com>
2010-10-29 10:52:25 +0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0'
+gem 'mysql2'
+gem 'unicorn'
+gem 'ruby-debug'
+
+group :test, :development do
+  gem "autotest"
+  gem "rspec-rails", "~> 2.0.1"
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ha::Application.config.secret_token = 'c47fa878386b377e78b8ccb97343b9a00a1ed8c56a69fc252d9db6c2e1ba8748d64cf66bb54a1bcf23f8e72d90da094689f0e5543f5d08fd1b1b5e04e88da56f'
