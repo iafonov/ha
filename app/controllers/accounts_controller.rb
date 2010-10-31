73cb2e84698ff4b74a4aa58082316abd2e1eb4b2
Igor Afonov <afonov@gmail.com>
2010-10-31 20:03:30 +0200

    Accounts list.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,8 @@
 class AccountsController < ApplicationController
   def index
+    respond_to do |format|
+      format.html
+      format.json  { render :json => Account.all }
+    end
   end
-
 end
--- public/javascripts/accounts.js
+++ public/javascripts/accounts.js
@@ -1,9 +1,103 @@
-
 window.Account = Backbone.Model.extend({
 })
 
 window.AccountsList = Backbone.Collection.extend({
-  model: Account
+    model: Account,
+    url: "/accounts"
 })
 
 window.Accounts = new AccountsList;
+
+window.AccountView = Backbone.View.extend({
+    tagName:  "li",
+    template: _.template($('#item-template').html()),
+
+    events: {
+        "dblclick div.account-name"          : "edit",
+        "click span.account-destroy"         : "clear",
+        "keypress .account-name-input"       : "updateOnEnter"
+    },
+
+    initialize: function() {
+        _.bindAll(this, 'render', 'close');
+        this.model.bind('change', this.render);
+        this.model.view = this;
+    },
+
+    render: function() {
+        $(this.el).html(this.template(this.model.toJSON()));
+        this.setContent();
+        return this;
+    },
+
+    setContent: function() {
+        var name = this.model.get('name');
+        this.$('.account-name').text(name);
+
+        this.input = this.$('.account-name-input');
+        this.input.bind('blur', this.close);
+        this.input.val(content);
+    },
+
+    edit: function() {
+        $(this.el).addClass("editing");
+        this.input.focus();
+    },
+
+    close: function() {
+        this.model.save({name: this.input.val()});
+        $(this.el).removeClass("editing");
+    },
+
+    updateOnEnter: function(e) {
+        if (e.keyCode == 13) this.close();
+    },
+
+    clear: function() {
+        this.model.clear();
+    }
+});
+
+window.AppView = Backbone.View.extend({
+    el: $("#accounts-manager"),
+
+    events: {
+        "keypress #new-account": "createOnEnter",
+        "click .todo-clear a"  : "clearCompleted"
+    },
+
+    initialize: function() {
+        _.bindAll(this, 'addOne', 'addAll', 'render');
+
+        this.input    = this.$("#new-account");
+
+        Accounts.bind('add',     this.addOne);
+        Accounts.bind('refresh', this.addAll);
+        Accounts.bind('all',     this.render);
+
+        Accounts.fetch();
+    },
+
+    addOne: function(account) {
+        var view = new AccountView({model: account});
+        this.$("#accounts-list").append(view.render().el);
+    },
+
+    addAll: function() {
+        Accounts.each(this.addOne);
+    },
+
+    newAttributes: function() {
+        return {
+            name: this.input.val()
+        };
+    },
+
+    createOnEnter: function(e) {
+        if (e.keyCode != 13) return;
+        Accounts.create(this.newAttributes());
+        this.input.val('');
+    }
+});
+
+window.App = new AppView;
