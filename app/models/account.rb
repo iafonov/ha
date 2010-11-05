c4a00a1483f3887c170e06af77de3967721a1907
Igor Afonov <afonov@gmail.com>
2010-11-05 10:58:12 +0200

    Corrected logic.

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,9 +4,22 @@ class Account < ActiveRecord::Base
   VALID_CURRENCIES = %w(USD UAH)
 
   validates_length_of :name, :minimum => 3
-  validates_uniqueness_of :name
+  validates_uniqueness_of :name, :scope => :currency
   validates_inclusion_of :currency, :in => VALID_CURRENCIES
 
   attr_accessible :name
   attr_accessible :currency
+
+  before_destroy :check_that_no_operations_exist
+
+
+private
+
+  def check_that_no_operations_exist
+    if operations.any?
+      errors.add(:base, "Account cannot be deleted because there are operations tied to it")
+      false
+    end
+  end
+
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,21 +1,39 @@
 require 'spec_helper'
 
 describe Account do
-  it "should allow only USD and UAH as currencies" do
-    Factory.build(:account, :currency => "UAH").should be_valid
-    Factory.build(:account, :currency => "USD").should be_valid
+  describe Account, "when first created" do
+    it "should allow only USD and UAH as currencies" do
+      Factory.build(:account, :currency => "UAH").should be_valid
+      Factory.build(:account, :currency => "USD").should be_valid
+    end
+
+    it "should always have currency" do
+      Factory.build(:account, :currency => "").should_not be_valid
+    end
+
+    it "should always have name" do
+      Factory.build(:account, :name => "").should_not be_valid
+    end
+
+    it "should have unique name in scope of currency" do
+      Factory.create(:account, :name => "test", :currency => "USD")
+      Factory.build(:account, :name => "test", :currency => "USD").should_not be_valid
+      Factory.build(:account, :name => "test", :currency => "UAH").should be_valid
+    end
   end
 
-  it "should always have currency" do
-    Factory.build(:account, :currency => "").should_not be_valid
-  end
+  describe Account, "with operations" do
+    before(:each) do
+      @account = Factory.create(:account, :name => "test")
+      Factory.create(:operation, :amount => Money.new(10), :account => @account)
+    end
 
-  it "should always have name" do
-    Factory.build(:account, :name => "").should_not be_valid
-  end
+    it "cannot be deleted" do
+      @account.destroy.should be_false
+      @account.errors.should_not be_empty
+    end
 
-  it "should have unique name" do
-    Factory.create(:account, :name => "test")
-    Factory.build(:account, :name => "test").should_not be_valid
+    it "shouldn't allow changing currency" do
+    end
   end
 end
