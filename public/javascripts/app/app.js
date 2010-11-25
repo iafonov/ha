2a9789710034f0b71af6dd74295e7c5bbd2c654c
Igor Afonov <afonov@gmail.com>
2010-11-25 17:46:36 +0200

    Dropped mustache.js

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -1,9 +1,3 @@
-Mustache.template = function(templateString) {
-    return function() { 
-        return Mustache.to_html(templateString, arguments[0], arguments[1]); 
-  };
-};
-
 _.templateSettings = {
     interpolate : /\{\{(.+?)\}\}/g
 };
