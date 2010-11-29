bee513be4dbc2cb8c892521d1b717c5d10e47d89
Igor Afonov <afonov@gmail.com>
2010-11-29 17:50:56 +0200

    Update currencies on start.

--- app/controllers/exchange_rates_controller.rb
+++ app/controllers/exchange_rates_controller.rb
@@ -1,11 +1,12 @@
 class ExchangeRatesController < ApplicationController
-  # stub
+
   def index
     rates = Hash.new
 
-    rates["USD_TO_UAH"] = 7.93
-    rates["UAH_TO_USD"] = (1.0 / 7.98)
+    rates["USD_TO_UAH"] = Money.default_bank.get_rate("USD", "UAH")
+    rates["UAH_TO_USD"] = Money.default_bank.get_rate("UAH", "USD")
 
     render :json => rates
   end
+
 end
---
+++ config/initializers/exhange_rates.rb
@@ -0,0 +1,3 @@
+# stub
+Money.add_rate("USD", "UAH", 7.93)
+Money.add_rate("UAH", "USD", (1 / 7.98))
